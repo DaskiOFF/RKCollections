458b22fb2d5095a56668783235a2801561402b61
Roman Kotov <waydeveloper@gmail.com>
2019-06-28 00:26:20 +0300

    Initial Commit

---
+++ RKCollections/RKCollections.h
@@ -0,0 +1,19 @@
+//
+//  RKCollections.h
+//  RKCollections
+//
+//  Created by Roman Kotov on 28/06/2019.
+//  Copyright © 2019 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKCollections.
+FOUNDATION_EXPORT double RKCollectionsVersionNumber;
+
+//! Project version string for RKCollections.
+FOUNDATION_EXPORT const unsigned char RKCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKCollections/PublicHeader.h>
+
+
